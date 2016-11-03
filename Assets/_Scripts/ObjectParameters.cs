9c78f4c4f5fd6f82278c23bebf94bd5d2668d009
Kevin Hunter <vudugrv@hotmail.com>
2016-11-03 15:12:38 -0700

    Lots of new map sections, plus triggers

---
+++ Assets/_Scripts/ObjectParameters.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectParameters : MonoBehaviour
+{
+    public bool canKinetic = true;
+    public bool canMass = false;
+    public bool canTorque = false;
+    public bool canGravity = false;
+
+    public float minMass = 1;
+    public float maxMass = 12;
+    public float minScale = 0.5f;
+    public float maxScale = 5;
+}
---
+++ Assets/_Scripts/Trigger_Door.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger_Door : MonoBehaviour
+{
+    public Transform door;
+
+    public float doorOffset = 3;
+    public float doorTotalTime = 4;
+    public bool moveUp = true;
+
+    public Vector3 doorStartPosition;
+    public Vector3 doorUpPosition, doorDownPosition;
+
+    private float doorElapsedTime;
+    private bool gateOn = false;
+    private bool gateRaised = false;
+
+    void Start()
+    {
+        doorStartPosition = door.localPosition;
+        doorUpPosition = door.localPosition + (Vector3.up * doorOffset);
+        doorDownPosition = door.localPosition + (Vector3.up * -doorOffset);
+    }
+
+    void Update()
+    {
+        if (gateOn && !gateRaised)
+        {
+            if (moveUp)
+                RaiseDoor();
+            else
+                LowerDoor();
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+        gateOn = true;
+    }
+
+    void RaiseDoor()
+    {
+        doorElapsedTime += Time.deltaTime;
+        door.localPosition = Vector3.Lerp(doorStartPosition, doorUpPosition, doorElapsedTime / doorTotalTime);
+
+        if (door.localPosition.y >= doorUpPosition.y)
+        {
+            gateRaised = true;
+            door.localPosition = doorUpPosition;
+        }
+    }
+
+    void LowerDoor()
+    {
+        doorElapsedTime += Time.deltaTime;
+        door.localPosition = Vector3.Lerp(doorStartPosition, doorDownPosition, doorElapsedTime / doorTotalTime);
+
+        if (door.localPosition.y <= doorDownPosition.y)
+        {
+            gateRaised = true;
+            door.localPosition = doorDownPosition;
+        }
+    }
+}
