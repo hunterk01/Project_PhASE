f2c65c4e0b771e50bd009229e50f1d88f491f4a9
Kevin Hunter <vudugrv@hotmail.com>
2016-11-04 17:29:50 -0700

    Added info and beam UI

---
+++ Assets/_Scripts/ObjectHandler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectHandler : MonoBehaviour
+{
+    public bool canKinetic = true;
+    public bool canMass = false;
+    public bool canTorque = false;
+    public bool canGravity = false;
+
+    public float minMass = 1;
+    public float maxMass = 12;
+    public float minScale = 0.5f;
+    public float maxScale = 5;
+
+    Vector3 vMinMass, vMaxMass;
+    Vector3 vMinScale, vMaxScale;
+
+    void Start()
+    {
+        vMinMass = new Vector3(minMass, minMass, minMass);
+        vMaxMass = new Vector3(maxMass, maxMass, maxMass);
+        vMinScale = new Vector3(minScale, minScale, minScale);
+        vMaxScale = new Vector3(maxScale, maxScale, maxScale);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void UpdateMass()
+    {
+
+    }
+}
---
+++ Assets/_Scripts/WeaponSelectUI.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WeaponSelectUI : MonoBehaviour
+{
+    public Text kineticText;
+    public Text massText;
+    public Text torqueText;
+    public Text gravityText;
+
+    public bool kineticEnabled = true;
+    public bool massEnabled = false;
+    public bool torqueEnabled = false;
+    public bool gravityEnabled = false;
+
+    public int activeWeapon = 1;
+
+    private Color disabledColor = Color.gray;
+    private Color enabledColor = Color.green;
+
+    void Start()
+    {
+        kineticText.color = enabledColor;
+        massText.color = disabledColor;
+        torqueText.color = disabledColor;
+        gravityText.color = disabledColor;
+    }
+
+    void Update()
+    {
+        SelectWeapon();
+    }
+
+    void SelectWeapon()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            activeWeapon = 1;
+            UpdateWeaponUI();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) && massEnabled)
+        {
+            activeWeapon = 2;
+            UpdateWeaponUI();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3) && torqueEnabled)
+        {
+            activeWeapon = 3;
+            UpdateWeaponUI();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4) && gravityEnabled)
+        {
+            activeWeapon = 4;
+            UpdateWeaponUI();
+        }
+    }
+
+    public void UpdateWeaponUI()
+    {
+        if (activeWeapon == 1)
+        {
+            kineticText.color = enabledColor;
+            massText.color = disabledColor;
+            torqueText.color = disabledColor;
+            gravityText.color = disabledColor;
+        }
+        else if (activeWeapon == 2)
+        {
+            kineticText.color = disabledColor;
+            massText.color = enabledColor;
+            torqueText.color = disabledColor;
+            gravityText.color = disabledColor;
+        }
+        else if (activeWeapon == 3)
+        {
+            kineticText.color = disabledColor;
+            massText.color = disabledColor;
+            torqueText.color = enabledColor;
+            gravityText.color = disabledColor;
+        }
+        else if (activeWeapon == 4)
+        {
+            kineticText.color = disabledColor;
+            massText.color = disabledColor;
+            torqueText.color = disabledColor;
+            gravityText.color = enabledColor;
+        }
+    }
+}
