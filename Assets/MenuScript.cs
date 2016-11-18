ed800ce6608c21d2992bec1109e45074801d7a52
kwmoore81 <kwmoore81@gmail.com>
2016-11-17 17:44:40 -0800

    Fixed Menu

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -6,6 +6,9 @@ public class MenuScript : MonoBehaviour {
     public float torqueForce = 10;
    
     void Start () {
+        
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
         rb.AddRelativeTorque(Vector3.up * torqueForce);
         rb.AddRelativeTorque(Vector3.right * torqueForce);
     }
